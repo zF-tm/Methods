c53cb02d034cd33c8f9aadb40ac3365c3a43f669
zF-tm <73698972+zF-tm@users.noreply.github.com>
2022-06-05 17:52:03 +0300

    Create Methods.sh

---
+++ Methods.sh
@@ -0,0 +1,262 @@
+#!/bin/bash
+x="3"
+while [ $x = 3 ]
+do
+clear
+echo "   _____              .___       __________         .__                          
+  /     \ _____     __| _/____   \______   \___.__. |__| ____      ___.__. ______
+ /  \ /  \\__  \   / __ |/ __ \   |    |  _<   |  | |  |/ ___\    <   |  |/  ___/
+/    Y    \/ __ \_/ /_/ \  ___/   |    |   \\___  | |  \  \___     \___  |\___ \ 
+\____|__  (____  /\____ |\___  >  |______  // ____| |__|\___  > /\ / ____/____  >
+        \/     \/      \/    \/          \/ \/              \/  \/ \/         \/ "
+sleep 3
+x="0"
+
+while [ $x = 0 ]
+do
+echo "choose your methods
+1. stockx
+2. xbox key method
+3. playstation cash
+4. unlimited virtual vccs
+x. leave "
+read choice
+	case "$choice" in
+		1)
+		clear
+		sleep 1
+		echo "   _____              .___       __________         .__                          
+  /     \ _____     __| _/____   \______   \___.__. |__| ____      ___.__. ______
+ /  \ /  \\__  \   / __ |/ __ \   |    |  _<   |  | |  |/ ___\    <   |  |/  ___/
+/    Y    \/ __ \_/ /_/ \  ___/   |    |   \\___  | |  \  \___     \___  |\___ \ 
+\____|__  (____  /\____ |\___  >  |______  // ____| |__|\___  > /\ / ____/____  >
+        \/     \/      \/    \/          \/ \/              \/  \/ \/         \/ "
+		echo "1. Go onto slilpp.xyz click Other ShoPPs on the top of the screen
+
+ 2. Search for StockX logins and buy one of your choice with a card linked
+
+ 3. Set your VPN or Socks to the zip of your drop address
+
+ 4. Log into the account and change the email and password (If you donâ€™t do this the account owner can just cancel the order)
+
+ 5. Now change the shipping address on the account to drop address of your choice.
+
+ 6. Look at items on the site for a little bit. Look like a real customer. Act like itâ€™s your account.
+
+ 7. Go to the item you want. Click Buy and then Buy Now. Checkout, ship to new drop address.
+
+ 8. Wait for shipment to arrive"
+x=1
+while [ $x = 1 ]
+do
+sleep 1
+echo "x - go back"
+read back
+case "$back" in
+x)
+clear
+echo "   _____              .___       __________         .__                          
+  /     \ _____     __| _/____   \______   \___.__. |__| ____      ___.__. ______
+ /  \ /  \\__  \   / __ |/ __ \   |    |  _<   |  | |  |/ ___\    <   |  |/  ___/
+/    Y    \/ __ \_/ /_/ \  ___/   |    |   \\___  | |  \  \___     \___  |\___ \ 
+\____|__  (____  /\____ |\___  >  |______  // ____| |__|\___  > /\ / ____/____  >
+        \/     \/      \/    \/          \/ \/              \/  \/ \/         \/ "
+x=0
+esac
+done
+;;
+2)
+clear
+echo "   _____              .___       __________         .__                          
+  /     \ _____     __| _/____   \______   \___.__. |__| ____      ___.__. ______
+ /  \ /  \\__  \   / __ |/ __ \   |    |  _<   |  | |  |/ ___\    <   |  |/  ___/
+/    Y    \/ __ \_/ /_/ \  ___/   |    |   \\___  | |  \  \___     \___  |\___ \ 
+\____|__  (____  /\____ |\___  >  |______  // ____| |__|\___  > /\ / ____/____  >
+        \/     \/      \/    \/          \/ \/              \/  \/ \/         \/ "
+sleep 1
+echo "Basic Information:
+-Non Carded
+-Socially Engineered
+
+Requirements:
+-Photoshop (I Recommend CS6 Because Its Easiest To Crack)
+-Unactivated Xbox Gift Cards(Walk Into A Store And Grab A Few And Put Them In Your Pocket Then Walk Out)
+-Fake Receipt(Included In The ZIP File)
+
+Optional But Recommended:
+-Printer For Printing Receipts
+
+
+Step One: Sign Up For A Microsoft Account (Dont Use Your Main As Mine Got A Two Week Ban For This)
+
+Step Two: Open The "Xbox Gift Card PSD.psd" With Photoshop
+
+Step Three: Take The Serial Number Off The Back Of One Of The Cards
+
+Step Four: Change The Serial Number Of The Receipt To The One From Step Three And Change The Date To Yesterday Date Anywhere Between 12PM-4PM
+
+Step Five: Go To File->Save As-> reciept.pdf
+
+Step Six: Visit -> https://beta.support.xbox.com/contact-us
+
+Step Seven: Click Contact Now
+
+Step Eight: Select Subscriptions & Billing
+
+Step Nine: I Cant Redeem A Code
+
+Step Ten: Click Forward
+
+Step Eleven: Select Chat Online
+
+Step Twelve: Wait For The Support Chat Agent
+
+Step Thirteen: Tell Them You Went Into Best Buy And Bought An Xbox Card And When You Went Home To Activate It, It Said The Code Was Invalid So You Called Up The Store And They Said To Contact Their Support Chat. (Keep To This Story Dont Break Character)
+
+Step Fourteen: They Will Ask For You To Take Pictures Of The Card (Front & Back) With The Case Number On Both Sides (Do So In Pencil So You Can Erase If Any Issues Arise, Make Sure To Include The Full Card Avoid Having Cropped Pictures ETC)
+
+Step Fifteen: They Will Also Ask For The Receipt (Upload It To The Same Link They Gave You From The Last Step
+Step Sixteen: Go Along With The Questions They Ask (If They Ask For A Physical Copy Of The Receipt U Can Either Save The Receipt As A PNG And Print It Cut It And Crinkle It Or Say You Dont Have A Physical Copy Of The Receipt)"
+x=1
+while [ $x = 1 ]
+do
+sleep 1
+echo "x - go back"
+read backtwo
+case "$backtwo" in
+x)
+clear
+sleep 1
+echo "   _____              .___       __________         .__                          
+  /     \ _____     __| _/____   \______   \___.__. |__| ____      ___.__. ______
+ /  \ /  \\__  \   / __ |/ __ \   |    |  _<   |  | |  |/ ___\    <   |  |/  ___/
+/    Y    \/ __ \_/ /_/ \  ___/   |    |   \\___  | |  \  \___     \___  |\___ \ 
+\____|__  (____  /\____ |\___  >  |______  // ____| |__|\___  > /\ / ____/____  >
+        \/     \/      \/    \/          \/ \/              \/  \/ \/         \/ "
+x=0
+esac
+done
+;;
+3)
+clear
+echo "   _____              .___       __________         .__                          
+  /     \ _____     __| _/____   \______   \___.__. |__| ____      ___.__. ______
+ /  \ /  \\__  \   / __ |/ __ \   |    |  _<   |  | |  |/ ___\    <   |  |/  ___/
+/    Y    \/ __ \_/ /_/ \  ___/   |    |   \\___  | |  \  \___     \___  |\___ \ 
+\____|__  (____  /\____ |\___  >  |______  // ____| |__|\___  > /\ / ____/____  >
+        \/     \/      \/    \/          \/ \/              \/  \/ \/         \/ "
+sleep 1
+echo "Head over to https://www.playstation.com/en-us/corporate/contactus  and go to Live 
+Chat. Type in your info. Click “PS4” and “Other”.
+
+Once in chat, greet the rep and be extremely nice. Make sure to sound very professional throughout your messages. 
+Tell the rep that you recently (anytime within 3 months) bought your son/daughter a PSN pass (any month) and that you're extremely 
+disappointed. Tell them that your child received extremely vulgar messages from other players, he/ your wife/ husband was really
+upset. Ask if there is any way 
+they can make it right (in your own words). The rep will talk with you for a bit and eventually “transfer you to a higher 
+authority” that deals with compensation.
+
+Once your with the new agent (in the same live chat) continue explaining to him your problem. Make sure to ask for some type of 
+compensation as well. The agent will eventually offer you a $5 PSN code. We are going to get more than that though. 
+Tell the rep you would like a $15 (or say $20, $30, $40, etc so we all don't say the same thing) and would be super grateful if
+you could get one. The rep will then tell you that the best he can offer is a $10 PSN credit code. Once he gives you the code, 
+thank the rep and exit the Live Chat."
+x=1
+while [ $x = 1 ]
+do
+sleep 1
+echo "x - go back"
+read backtwo
+case "$backtwo" in
+x)
+clear
+echo "   _____              .___       __________         .__                          
+  /     \ _____     __| _/____   \______   \___.__. |__| ____      ___.__. ______
+ /  \ /  \\__  \   / __ |/ __ \   |    |  _<   |  | |  |/ ___\    <   |  |/  ___/
+/    Y    \/ __ \_/ /_/ \  ___/   |    |   \\___  | |  \  \___     \___  |\___ \ 
+\____|__  (____  /\____ |\___  >  |______  // ____| |__|\___  > /\ / ____/____  >
+        \/     \/      \/    \/          \/ \/              \/  \/ \/         \/ "
+sleep 1
+x=0
+esac
+done
+;;
+4)
+clear
+echo "   _____              .___       __________         .__                          
+  /     \ _____     __| _/____   \______   \___.__. |__| ____      ___.__. ______
+ /  \ /  \\__  \   / __ |/ __ \   |    |  _<   |  | |  |/ ___\    <   |  |/  ___/
+/    Y    \/ __ \_/ /_/ \  ___/   |    |   \\___  | |  \  \___     \___  |\___ \ 
+\____|__  (____  /\____ |\___  >  |______  // ____| |__|\___  > /\ / ____/____  >
+        \/     \/      \/    \/          \/ \/              \/  \/ \/         \/ "
+sleep 1
+echo "Self introduction:
+
+Call me Chase, I am from United States, and been doing this "social engineering" thing for about 2 years now. Not the type to brag or leak or even share information unless you seem like a genuine person who is actually motivated, realistic, and somewhat intelligent.
+
+Requirements for this Virtual Credit Card:
+
+1. Legitimate information
+2. Be able to apply for a credit card
+3. Actually pay off any debt you spend on the card
+4. Get approved for the credit card
+
+How this works:
+
+1. go to a bank of america office or go to bankofamerica.com
+2. apply for their credit card
+3. once you are approved for the credit card, you will be able to generate unlimited amounts of card numbers
+
+Pros of BOA generated cards:
+
+1. You can add expiration date
+2. You can add a card limit ($2 minimum, credit limit maximum)
+3. Unlimited amount of credit card numbers
+4. 100% valid card numbers and works for almost everything: gamefly, netflix, ebay, paypal, google adword, facebook ad, and many many other trial places
+5. Different billing names can charge through
+6. INSTANT card number creation
+7. Can file chargeback with credit card directly
+8. too many more, explore it yourselves
+
+
+Cons of BOA generated numbers:
+
+1. they all start with the same 4 digits.
+2. they are all linked to your actual credit card number (so if you loan someone else a vcc, they charge it, you get fucked)
+3. they all share the same billing address
+4. you have to pay the amount of money due if you fuck up
+5. sometimes it can over charge the limit ($5 card limit, charge $9 will go through)
+
+-Enjoy, and don't abuse or over leak this shit."
+x=1
+while [ $x = 1 ]
+do
+sleep 1
+echo "x - go back"
+read backtwo
+case "$backtwo" in
+x)
+clear
+echo "   _____              .___       __________         .__                          
+  /     \ _____     __| _/____   \______   \___.__. |__| ____      ___.__. ______
+ /  \ /  \\__  \   / __ |/ __ \   |    |  _<   |  | |  |/ ___\    <   |  |/  ___/
+/    Y    \/ __ \_/ /_/ \  ___/   |    |   \\___  | |  \  \___     \___  |\___ \ 
+\____|__  (____  /\____ |\___  >  |______  // ____| |__|\___  > /\ / ____/____  >
+        \/     \/      \/    \/          \/ \/              \/  \/ \/         \/ "
+x=0
+esac
+done
+;;
+x)
+echo "exiting."
+sleep 1
+echo "exiting.."
+sleep 1
+echo "exiting..."
+sleep 2
+exit
+x=1
+esac
+done
+
+done
